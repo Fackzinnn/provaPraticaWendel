37c1f62be5bd3bd3727c03eae79d0ad89d7d2e94
WENDEL GABRIEL HANG <wendel_hang@estudante.sesisenai.org.br>
2024-07-04 11:10:40 -0300

    first commit

---
+++ provaPratica/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/senai.png" alt="Senai Logo" className="senai-logo" />
+      <h1>Wendel Gabriel Hang 3c
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
